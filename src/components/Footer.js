633728d056b92f693663774d879d9ee60c77da2b
k.dubov <example@example.com>
2022-04-08 18:56:25 +0300

    Add Home, Header, Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,124 @@
+import React from 'react'
+
+export const Footer = () => {
+  return (
+
+    <footer id="footer" className="site-footer" itemScope="itemscope" itemType="https://schema.org/WPFooter" role="contentinfo">
+      <div id="footer-inner" className="clr">
+        <div id="footer-widgets" className="oceanwp-row clr">
+          <div className="footer-widgets-inner">
+            <div data-elementor-type="wp-post" data-elementor-id={150} className="elementor elementor-150" data-elementor-settings="[]">
+              <div className="elementor-section-wrap">
+                <section className="elementor-section elementor-top-section elementor-element elementor-element-cdf272e elementor-section-boxed elementor-section-height-default elementor-section-height-default" data-id="cdf272e" data-element_type="section" data-settings="{&quot;background_background&quot;:&quot;gradient&quot;}">
+                  <div className="elementor-container elementor-column-gap-default">
+                    <div className="elementor-column elementor-col-100 elementor-top-column elementor-element elementor-element-58b3257" data-id="58b3257" data-element_type="column">
+                      <div className="elementor-widget-wrap elementor-element-populated">
+                        <div className="elementor-element elementor-element-f66ff03 elementor-widget elementor-widget-wp-widget-ocean_custom_menu" data-id="f66ff03" data-element_type="widget" data-widget_type="wp-widget-ocean_custom_menu.default">
+                          <div className="elementor-widget-container">
+                            <style type="text/css" dangerouslySetInnerHTML={{ __html: "\n                                                            .ocean_custom_menu-REPLACE_TO_ID>ul>li>a,\n                                                            .custom-menu-widget .ocean_custom_menu-REPLACE_TO_ID .dropdown-menu .sub-menu li a.menu-link {\n                                                                color: #000000;\n                                                                text-transform: uppercase;\n                                                            }\n\n                                                            .custom-menu-widget .ocean_custom_menu-REPLACE_TO_ID.oceanwp-custom-menu>ul.click-menu .open-this {\n                                                                color: #000000;\n                                                            }\n\n                                                            .ocean_custom_menu-REPLACE_TO_ID>ul>li>a:hover,\n                                                            .custom-menu-widget .ocean_custom_menu-REPLACE_TO_ID .dropdown-menu .sub-menu li a.menu-link:hover {\n                                                                color: #ffffff;\n                                                            }\n\n                                                            .custom-menu-widget .ocean_custom_menu-REPLACE_TO_ID.oceanwp-custom-menu>ul.click-menu .open-this:hover {\n                                                                color: #ffffff;\n                                                            }\n                                                        " }} />
+                            <div className="oceanwp-custom-menu clr ocean_custom_menu-REPLACE_TO_ID center dropdown-hover">
+                              <ul id="menu-menju-podvala" className="dropdown-menu sf-menu">
+                                <li id="menu-item-43" className="menu-item menu-item-type-post_type menu-item-object-page menu-item-home current-menu-item page_item page-item-2 current_page_item menu-item-43">
+                                  <a href="index.html" className="menu-link">Автосервис РиаМоторс</a>
+                                </li>
+                                <li id="menu-item-1162" className="menu-item menu-item-type-post_type menu-item-object-page menu-item-1162">
+                                  <a href="index-196.html" className="menu-link">Гарантия</a></li>
+                                <li id="menu-item-1163" className="menu-item menu-item-type-post_type menu-item-object-page menu-item-1163">
+                                  <a href="index-194.html" className="menu-link">Обслуживание юридических
+                                    лиц</a></li>
+                                <li id="menu-item-1164" className="menu-item menu-item-type-post_type menu-item-object-page menu-item-1164">
+                                  <a href="index-195.html" className="menu-link">Оплата</a></li>
+                                <li id="menu-item-44" className="menu-item menu-item-type-post_type menu-item-object-page menu-item-44">
+                                  <a href="index-199.html" className="menu-link">Контакты</a></li>
+                              </ul>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </section>
+                <section className="elementor-section elementor-top-section elementor-element elementor-element-5922053 elementor-section-boxed elementor-section-height-default elementor-section-height-default" data-id={5922053} data-element_type="section" data-settings="{&quot;background_background&quot;:&quot;gradient&quot;}">
+                  <div className="elementor-container elementor-column-gap-default">
+                    <div className="elementor-column elementor-col-33 elementor-top-column elementor-element elementor-element-6269445" data-id={6269445} data-element_type="column">
+                      <div className="elementor-widget-wrap elementor-element-populated">
+                        <div className="elementor-element elementor-element-a3596e0 elementor-widget elementor-widget-text-editor" data-id="a3596e0" data-element_type="widget" data-widget_type="text-editor.default">
+                          <div className="elementor-widget-container">
+                            <a title="Ремонт двигателя" href="index-45.html">Ремонт
+                              двигателя</a><br />
+                            <a title="Ремонт и проверка тормозных усилий" href="index-55.html">Ремонт и проверка тормозных
+                              усилий</a>
+                            <br />
+                            <a title="Проточка тормозных дисков" href="index-55.html">Проточка тормозных дисков</a>
+                            <br />
+                            <a title="Шиномонтаж и балансировка" href="index-81.html">Шиномонтаж и балансировка</a>
+                            <br />
+                            <a title="Диагностика технического состояния" href="index-5.html">Диагностика технического
+                              состояния</a>
+                            <br />
+                            <a title="Развал-схождение" href="index-24.html">Развал-схождение</a>
+                            <br />
+                            <a title="Заправка автомобильных кондиционеров" href="index-29.html">Заправка автомобильных
+                              кондиционеров</a>
+                            <br />
+                            <a title="Ремонт тормозной системы" href="index-55.html">Ремонт тормозной системы</a>
+                            <br />
+                            <a title="Ремонт электрооборудования" href="index-30.html">Ремонт электрооборудования</a>
+                            <br />
+                            <a title="Ремонт подвески и ходовой части" href="index-63.html">Ремонт подвески и ходовой части</a>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="elementor-column elementor-col-33 elementor-top-column elementor-element elementor-element-5f08d05" data-id="5f08d05" data-element_type="column">
+                      <div className="elementor-widget-wrap elementor-element-populated">
+                        <div className="elementor-element elementor-element-450fac5 elementor-widget elementor-widget-text-editor" data-id="450fac5" data-element_type="widget" data-widget_type="text-editor.default">
+                          <div className="elementor-widget-container">
+                            <strong>ООО «РиаМоторс» </strong>—<br />
+                            Станция технического обслуживания<br />
+                            Рено, Пежо, Ситроен, Лада, Фольксваген, Киа, Шкода, Тойота,
+                            Ниссан, Хендай </div>
+                        </div>
+                        <div className="elementor-element elementor-element-92be8ea elementor-widget elementor-widget-image" data-id="92be8ea" data-element_type="widget" data-widget_type="image.default">
+                          <div className="elementor-widget-container">
+                            <a href="index.html">
+                              <img width={170} height={86} src="images/logo.png" className="attachment-large size-large" alt="Автосервис РиаМоторс" loading="lazy" title="Автосервис РиаМоторс" /> </a>
+                          </div>
+                        </div>
+                        <div className="elementor-element elementor-element-edadf1b elementor-widget elementor-widget-text-editor" data-id="edadf1b" data-element_type="widget" data-widget_type="text-editor.default">
+                          <div className="elementor-widget-container">
+                            <p>Copyright 2022 — Все права защищены</p>
+                            <p><a href="index-200.html">Политика обработки персональных
+                              данных</a></p>
+                            <p><a href="https://new-trait.by/">Разработка и продвижение
+                              сайта — New Trait</a></p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="elementor-column elementor-col-33 elementor-top-column elementor-element elementor-element-6ab8f2a" data-id="6ab8f2a" data-element_type="column">
+                      <div className="elementor-widget-wrap elementor-element-populated">
+                        <div className="elementor-element elementor-element-eeb61cd elementor-widget elementor-widget-text-editor" data-id="eeb61cd" data-element_type="widget" data-widget_type="text-editor.default">
+                          <div className="elementor-widget-container">
+                            <p>Наши контакты:<br />г. Минск, ул. Стебенева, 12<br /><a href="tel:+375173658393">+375 17
+                              365-83-93</a><br /><a href="tel:+375293726365">+375
+                                29 372 63 65</a><br /><a href="tel:+375298726365">+375 29 872 63
+                                  65</a><br />e-mail: riamotors@mail.ru<br />Режим
+                              работы : пн.-пт. 8.00 – 20.00<br />УНП
+                              190845207<br />Свидетельство регистрации в<br />реестре
+                              бытовых услуг от 14.12.2017</p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </section>
+              </div>
+            </div>
+          </div>{/* .container */}
+        </div>{/* #footer-widgets */}
+      </div>{/* #footer-inner */}
+    </footer>
+
+  )
+}
---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from "../components/Header";
+import { Footer } from "../components/Footer";
+
+const Home = () => {
+  return (
+    <div class="home page-template page-template-elementor_header_footer page page-id-2 wp-custom-logo wp-embed-responsive no-lightbox oceanwp-theme sidebar-mobile content-full-width content-max-width page-header-disabled has-breadcrumbs elementor-default elementor-template-full-width elementor-kit-10 elementor-page elementor-page-2">
+      <Header />
+      home
+      <Footer />
+    </div>
+  )
+}
+
+export default Home
